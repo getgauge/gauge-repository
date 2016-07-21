4682edb4de476dedad0ebb366f5d6c5f47f9c0fb
sriv <srikanth.ddit@gmail.com>
2016-07-21 16:41:26 +0530

    fix ruby templates for nightly Gemfile
    - remove nightly Gemfile generation from ruby_selenium, it uses webdriver gems

--- templates/ruby_selenium/create_gemfile.rb
+++
@@ -1,6 +0,0 @@
-require 'json'
-version=JSON.parse(`gauge -v --machine-readable`)['plugins'].find {|x| x['name'] == 'ruby'}['version'] rescue nil
-if !version.nil? && version.include?("nightly")
-    v=version.split('.nightly').first
-    File.open("Gemfile", 'w') { |file| file.write("gem 'gauge-ruby', '~>#{v}', :github => 'getgauge/gauge-ruby', :ref => 'HEAD', :group => [:development, :test]\n") }
-end
