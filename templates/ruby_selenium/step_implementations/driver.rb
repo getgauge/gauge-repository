ed02522cb0ce081f79b2366a6a2597f454ae886b
Gas unknown developer <gas@gas.com>
2017-03-07 11:48:45 +0700

    linted ruby code using rubocop standard

--- templates/ruby_selenium/step_implementations/driver.rb
+++ templates/ruby_selenium/step_implementations/driver.rb
@@ -1,18 +1,20 @@
 require 'selenium-webdriver'
 
+# Driver module for selenium web driver
 module Driver
-    def driver
-        @@driver
-    end
-    
-    before_suite do
-        # init driver from DriverFactory
-        # set the "browser" env variable in one of the properties files. check env/default/default.properties
-        @@driver = Selenium::WebDriver.for ENV["browser"].to_sym
-    end
+  def driver
+    @@driver
+  end
 
-    after_suite do
-        # quit driver
-        @@driver.quit
-    end
+  before_suite do
+    # init driver from DriverFactory
+    # set the "browser" env variable in one of the properties files.
+    # check env/default/default.properties
+    @@driver = Selenium::WebDriver.for ENV['browser'].to_sym
+  end
+
+  after_suite do
+    # quit driver
+    @@driver.quit
+  end
 end
