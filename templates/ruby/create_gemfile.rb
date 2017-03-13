fe2632a7d113d9cbb02fa954f9315f8117c3437b
sriv <srikanth.ddit@gmail.com>
2017-03-13 17:25:20 +0530

    fixed rubocop - ruby templates
    pr #6 had issues when gauge-ruby isnt installed
    
    This reverts commit ed02522cb0ce081f79b2366a6a2597f454ae886b.

--- templates/ruby/create_gemfile.rb
+++ templates/ruby/create_gemfile.rb
@@ -1,27 +1,35 @@
 require 'json'
-plugins = JSON.parse(`gauge -v --machine-readable`)['plugins']
-unless plugins.nil?
-  ruby_plugin = plugins.find do |x|
+
+begin
+  gauge_version = `gauge -v --machine-readable`
+  gauge_ruby_info = JSON.parse(gauge_version)['plugins'].find do |x|
     x['name'] == 'ruby'
-  end || {}
-  version = ruby_plugin['version']
+  end
+  version = gauge_ruby_info['version']
+rescue
+  version = nil
 end
-if version.to_s.include?('nightly') || ENV['GAUGE_SOURCE_BUILD'] == 'true'
+
+source_build = ENV['GAUGE_SOURCE_BUILD'] == 'true'
+
+if source_build || (!version.nil? && version.include?('nightly'))
   v = version.split('.nightly').first
   File.open('Gemfile', 'w') do |file|
-    file.write(
-      "gem 'test-unit', group: [:development, :test]\n"\
-      "gem 'gauge-ruby', '~>#{v}', github: 'getgauge/gauge-ruby',"\
-      " ref: 'HEAD', group: [:development, :test]\n"
-    )
+    gemfile_content <<-eot
+    gem 'test-unit', :group => [:development, :test]
+    gem 'gauge-ruby', '~>#{v}', :github => 'getgauge/gauge-ruby', :ref => 'HEAD', :group => [:development, :test]
+    eot
+    file.write(gemfile_content)
   end
 else
   File.open('Gemfile', 'w') do |file|
-    file.write(
-      "source 'https://rubygems.org'\n\n"\
-      "gem 'test-unit', group: [:development, :test]\n"\
-      "gem 'gauge-ruby', '~>#{version}', group: [:development, :test]\n"
-    )
+    gemfile_content <<-eot
+    source "https://rubygems.org"
+
+    gem 'test-unit', :group => [:development, :test]
+    gem 'gauge-ruby', '~>#{version}', :group => [:development, :test]
+    eot
+    file.write(gemfile_content)
   end
 end
 
