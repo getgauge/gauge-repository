a65d7defeba58ab19147e776aa5842e0b09ebcd0
sriv <srikanth.ddit@gmail.com>
2017-03-13 20:35:22 +0530

    fixed class var @driver is nil in ruby_selenium template

--- templates/ruby_selenium/step_implementations/driver.rb
+++ templates/ruby_selenium/step_implementations/driver.rb
@@ -3,14 +3,14 @@ require 'selenium-webdriver'
 # Driver module for selenium web driver
 module Driver
   def driver
-    @driver
+    @@driver
   end
 
   before_suite do
     # init driver from DriverFactory
     # set the "browser" env variable in one of the properties files.
     # check env/default/default.properties
-    @driver = Selenium::WebDriver.for ENV['browser'].to_sym
+    @@driver = Selenium::WebDriver.for ENV['browser'].to_sym
   end
 
   after_suite do
