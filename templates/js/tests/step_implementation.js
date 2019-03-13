7ad8d4bfda19d6e7840e4d2134946510f299b6e9
--edit <s.chauhan5505@gmail.com>
2019-03-13 15:11:15 +0530

    Removed name selector from js template

--- templates/js/tests/step_implementation.js
+++ templates/js/tests/step_implementation.js
@@ -1,6 +1,6 @@
 /* globals gauge*/
 "use strict";
-const { openBrowser,write, closeBrowser, goto, press, text, focus, inputField } = require('taiko');
+const { openBrowser,write, closeBrowser, goto, press, text, focus, inputField, toRightOf } = require('taiko');
 const assert = require("assert");
 const headless = process.env.headless_chrome.toLowerCase() === 'true';
 
@@ -16,12 +16,12 @@ step("Goto getgauge github page", async () => {
     await goto('https://github.com/getgauge');
 });
 
-step("Search for <Taiko>", async (query) => {
-    await focus(inputField({'name': 'q'}))
+step("Search for <query>", async (query) => {
+    await focus(inputField(toRightOf('Pricing')))
     await write(query);
     await press('Enter');
 });
 
-step("Page contains <getgauge/taiko>", async (content) => {
+step("Page contains <content>", async (content) => {
     assert.ok(await text(content).exists());
 });
