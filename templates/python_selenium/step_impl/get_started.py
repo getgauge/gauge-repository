cf7f14e2d105c9bfad4731576378187876c398c1
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2020-04-16 19:24:52 +0100

    Turn on python selenium screenshot
    Signed-off-by: Zabil Cheriya Maliackal <zabilcm@gmail.com>

--- templates/python_selenium/step_impl/get_started.py
+++ templates/python_selenium/step_impl/get_started.py
@@ -3,6 +3,8 @@ from getgauge.python import before_suite, after_suite, step
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options  
 from selenium.webdriver.common.keys import Keys
+from uuid import uuid1
+from getgauge.python import custom_screenshot_writer
 
 @before_suite
 def init():
@@ -27,3 +29,12 @@ def go_to_get_started_page(query):
 @step("Go to Google homepage at <url>")
 def go_to_gauge_homepage_at(url):
     driver.get(url)
+
+# Return a screenshot file name
+@custom_screenshot_writer
+def take_screenshot():
+    image = driver.get_screenshot_as_png()
+    file_name = os.path.join(os.getenv("gauge_screenshots_dir"), "screenshot-{0}.png".format(uuid1().int))
+    file = open(file_name, "wb")
+    file.write(image)
+    return os.path.basename(file_name)
